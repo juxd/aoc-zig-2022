3bb6e022f8a5b9f7f33b8607e8ba55dd6d9548b5
JN <jluuiss@gmail.com>
2022-12-11 14:58:32 +0800

    solve day 2

--- src/ex2.zig
+++ src/ex2.zig
@@ -2,20 +2,104 @@ const std = @import("std");
 const file_reader = @import("file_reader.zig");
 
 const RockPaperScissors = struct {
-    const Choice = enum(i8) { rock = 0, paper = 1, scissors = 2 };
-    const Outcome = enum { win, draw, lose };
+    const Choice = enum(i8) {
+        rock = 0,
+        paper = 1,
+        scissors = 2,
 
-    pub fn outcomeForB(a: Choice, b: Choice) Outcome {
-        switch (@mod(@enumToInt(b) - @enumToInt(a), 3)) {
-            0 => return Outcome.draw,
-            1 => return Outcome.win,
-            2 => return Outcome.lose,
-            else => unreachable,
+        const Self = @This();
+
+        fn toScore(self: Self) i32 {
+            return switch (self) {
+                Self.rock => 1,
+                Self.paper => 2,
+                Self.scissors => 3,
+            };
         }
+
+        fn fromOrdinal(ord: i8) !Self {
+            return switch(ord) {
+                0 => Self.rock,
+                1 => Self.paper,
+                2 => Self.scissors,
+                else => error.UnrecognizedOrdinal
+            };
+        }
+    };
+
+    const Outcome = enum(i8) {
+        win = 1,
+        draw = 0,
+        lose = 2,
+
+        const Self = @This();
+
+        fn toScore(self: Self) i32 {
+           return switch (self) {
+                Self.win => 6,
+                Self.lose => 0,
+                Self.draw => 3,
+            };
+        }
+    };
+
+    fn outcomeForB(a: Choice, b: Choice) Outcome {
+        return switch (@mod(@enumToInt(b) - @enumToInt(a), 3)) {
+            0 => Outcome.draw,
+            1 => Outcome.win,
+            2 => Outcome.lose,
+            else => unreachable,
+        };
+    }
+
+    fn fromElf(char: u8) Choice {
+        return switch (char) {
+            'A' => Choice.rock,
+            'B' => Choice.paper,
+            'C' => Choice.scissors,
+            else => unreachable
+        };
+    }
+
+    fn fromYou(char: u8) Choice {
+        return switch (char) {
+            'X' => Choice.rock,
+            'Y' => Choice.paper,
+            'Z' => Choice.scissors,
+            else => unreachable
+        };
+    }
+
+    fn outcomeNeeded(char: u8) Outcome {
+        return switch (char) {
+            'X' => Outcome.lose,
+            'Y' => Outcome.draw,
+            'Z' => Outcome.win,
+            else => unreachable
+        };
+    }
+
+    fn choiceNeeded(elf: Choice, targetOutcome: Outcome) Choice {
+        return Choice.fromOrdinal(@mod((@enumToInt(elf) + @enumToInt(targetOutcome)), 3)) catch unreachable;
+    }
+
+    fn score(elf: u8, you: u8) i32 {
+        const yourChoice = fromYou(you);
+        const choiceScore: i32 = yourChoice.toScore();
+        const outcomeScore: i32 = outcomeForB(fromElf(elf), yourChoice).toScore();
+        return choiceScore + outcomeScore;
+    }
+
+    fn score2(elf: u8, you: u8) i32 {
+        const targetOutcome = outcomeNeeded(you);
+        const yourChoice = choiceNeeded(fromElf(elf), targetOutcome);
+        const choiceScore = yourChoice.toScore();
+        const outcomeScore = targetOutcome.toScore();
+        return choiceScore + outcomeScore;
     }
 };
 
-test {
+test "outcome for two" {
     const Choice = RockPaperScissors.Choice;
     const Outcome = RockPaperScissors.Outcome;
 
@@ -27,6 +111,17 @@ test {
     try expectEqual(RockPaperScissors.outcomeForB(Choice.scissors, Choice.rock), Outcome.win);
 }
 
+test "outcome from target" {
+    const Choice = RockPaperScissors.Choice;
+    const Outcome = RockPaperScissors.Outcome;
+
+    const expectEqual = std.testing.expectEqual;
+
+    try expectEqual(RockPaperScissors.choiceNeeded(Choice.scissors, Outcome.win), Choice.rock);
+    try expectEqual(RockPaperScissors.choiceNeeded(Choice.scissors, Outcome.draw), Choice.scissors);
+    try expectEqual(RockPaperScissors.choiceNeeded(Choice.scissors, Outcome.lose), Choice.paper);
+}
+
 fn part1(filename: []const u8) anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -34,11 +129,16 @@ fn part1(filename: []const u8) anyerror!void {
     var reader = try file_reader.FileReader.init(filename);
     defer reader.deinit();
 
+    var score: i32 = 0;
+
     while (try reader.getNextLine(allocator, 1000)) |line| {
         defer allocator.free(line);
+        const line_score = RockPaperScissors.score(line[0], line[2]);
+        std.debug.print("scoring: {s} = {d}\n", .{ line, line_score });
+        score += line_score;
     }
 
-    std.debug.print("answer is {d}\n", .{ 0 });
+    std.debug.print("answer is {d}\n", .{ score });
 }
 
 fn part2(filename: []const u8) anyerror!void {
@@ -48,12 +148,16 @@ fn part2(filename: []const u8) anyerror!void {
     var reader = try file_reader.FileReader.init(filename);
     defer reader.deinit();
 
+    var score: i32 = 0;
+
     while (try reader.getNextLine(allocator, 1000)) |line| {
         defer allocator.free(line);
-
+        const line_score = RockPaperScissors.score2(line[0], line[2]);
+        std.debug.print("scoring: {s} = {d}\n", .{ line, line_score });
+        score += line_score;
     }
 
-    std.debug.print("answer is {d}\n", .{ 0 });
+    std.debug.print("answer is {d}\n", .{ score });
 }
 
 pub fn f(part: usize, filename: []const u8) anyerror!void {
